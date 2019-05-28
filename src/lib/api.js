fdbe16cf7b2b13b8c3c14e9b565733e71e0cc784
Paul Leavitt <pleavitt@gmail.com>
2019-05-28 20:33:21 +1000

    Code cleanup

--- src/lib/api.js
+++
@@ -1,7 +0,0 @@
-export function getWeather() {
-      fetch(
-        'https://api.openweathermap.org/data/2.5/weather?q=London&APPID=82f64da34251b0e58e057d344f3307a1'
-      ).then(data => data.json())
-      .then( result => result)
-      .catch(error => console.error(error));
-  }
